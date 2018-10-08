046acbad94b355129df3f27c347526ec9cb100db
Jim I McCallson <ghimo@familysearch.org>
2018-10-08 11:57:31 -0600

    this is to get it all back in one place

--- src/test/java/streamy/WinnerTest.java
+++ src/test/java/streamy/WinnerTest.java
@@ -20,6 +20,50 @@ import static streamy.UsersData.userList;
  */
 class WinnerTest {
   @Test
+  public void testPeek(){
+    Stream<UsersData> streamUsersData = userList.stream();
+
+    streamUsersData.peek(e -> System.out.println(e.getSalary()))
+      .collect(Collectors.toList());
+  }
+
+  @Test
+  public void testChangeValue(){
+    Stream<UsersData> streamUsersData = userList.stream();
+
+    streamUsersData.peek(e -> e.salaryIncrement(e, 10L))
+      .peek(e -> System.out.println(e.getSalary()))
+      .collect(Collectors.toList());
+  }
+
+  @Test
+  public void testShowValue(){
+    Stream<UsersData> streamUsersData = userList.stream();
+
+    streamUsersData.peek(e -> System.out.println(e.getSalary()))
+      .collect(Collectors.toList());
+  }
+
+  @Test
+  public void testStoreValue(){
+    List<Long> resultSortedSalary = new ArrayList<>();
+    Stream<UsersData> streamUsersData = userList.stream();
+
+    streamUsersData.peek(e -> resultSortedSalary.add(e.getSalary()))
+      .peek(e -> System.out.println(resultSortedSalary))
+      .collect(Collectors.toList());
+  }
+
+  @Test
+  public void testNumberGenerator(){
+    Stream.iterate(1, (Integer n) -> n + 1)
+      .peek(n -> System.out.println("number generated: - " + n))
+      .filter(n -> (n % 2 == 0))
+      .peek(n -> System.out.println("Even number filter passed for - " + n))
+      .limit(5)
+      .count();
+  }
+  @Test
   void winnersOfToursLessThan3500km() {
     // Filter and Map -
     List<String> winnersOfToursLessThan3500km = null;
